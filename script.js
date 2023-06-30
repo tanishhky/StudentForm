74e838b78e25c6835b68e7f41a8da737be02c828
Tanishk Yadav <87553028+tanishhky@users.noreply.github.com>
2023-06-30 12:59:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,196 @@
-function updateOptions() {
-    var branch = document.getElementById("branch").value;
-    var dropdown = document.getElementById("year");
-  
-    dropdown.innerHTML = "";
-    var opt = document.createElement("option");
-    opt.text = "--Select your Year--";
-    dropdown.add(opt);
-  
-    if (branch == "BTech") {
-      for (var i = 1; i <= 4; i++) {
-        var opt = document.createElement("option");
-        opt.text = i;
-        dropdown.add(opt);
-      }
-    } else if (branch == "BSc" || branch == "BBA") {
-      for (var i = 1; i < 4; i++) {
-        var opt = document.createElement("option");
-        opt.text = i;
-        dropdown.add(opt);
-      }
-    } else if (branch == "Mtech" || branch == "MSc" || branch == "MBA") {
-      for (let i = 1; i < 3; i++) {
-        var opt = document.createElement("option");
-        opt.text = i;
-        dropdown.add(opt);
-      }
+var today = new Date().toISOString().split("T")[0];
+document.getElementById("dateInput").min = today;
+
+var link = document.getElementById("lynk");
+link.removeAttribute("href");
+
+function validateUUID() {
+    var aid = document.getElementById("aipId").value;
+    var reg =
+        /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    var validationMessage = document.getElementById("uuidValidationMessage");
+
+    if (reg.test(aid)) {
+        validationMessage.innerText = "";
+        return true;
+    } else {
+        validationMessage.innerText = "Invalid UUID";
+        return false;
     }
-  }
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    var branchDropdown = document.getElementById("branch");
-    if (branchDropdown) {
-      branchDropdown.addEventListener("change", updateOptions);
+    validateForm();
+}
+function validateAIUID() {
+    var aid = document.getElementById("aiuId").value;
+    var reg =
+        /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    var validationMessage = document.getElementById("AIUIDValidationMessage");
+
+    if (reg.test(aid)) {
+        validationMessage.innerText = "";
+        return true;
+    } else {
+        validationMessage.innerText = "Invalid UUID";
+        return false;
     }
-  }); // <-- Added closing parenthesis and semicolon
-  
-  var studentForm = document.getElementById("studentForm");
-  if (studentForm) {
-    studentForm.addEventListener("submit", function(e) {
-      e.preventDefault();
-  
-      //var imag = btoa(document.getElementById("pf").querySelector("input").value);
-      var fName = document.getElementById("fn").querySelector("input").value;
-      var mName = document.getElementById("mn").querySelector("input").value;
-      var lName = document.getElementById("ln").querySelector("input").value;
-      var mail = document.getElementById("mail").querySelector("input").value;
-      var branch = document.getElementById("branch").value;
-      var year = document.getElementById("year").value;
-  
-      var stuData = {
-        //"Image": imag,
-        "Name": fName + ' ' + mName + ' ' + lName,
-        "email": mail,
-        "Branch": branch,
-        "Year": year
-      };
-  
-      // Convert stuData to JSON
-      var jsonData = JSON.stringify(stuData);
-      console.log(jsonData);
-  
-      // Additional code to save or process the JSON data as needed
-  
-      // Reset the form
-      studentForm.reset();
-    });
-  }
-  
-  function previewImage(event) {
-    var reader = new FileReader();
-    reader.onload = function() {
-      var output = document.getElementById('preview');
-      output.src = reader.result;
-    };
-    reader.readAsDataURL(event.target.files[0]);
-  }
-  
+    validateForm();
+}
+
+function validateEmail() {
+    var emailInput = document.getElementById("aipEmail");
+    var validationMessage = document.getElementById("emailValidationMessage");
+    var submitButton = document.getElementById("submitButton");
+
+    if (emailInput.validity.valid) {
+        validationMessage.innerText = "";
+        emailInput.classList.remove("is-invalid");
+        return true;
+    } else {
+        validationMessage.innerText = "Invalid email address";
+        emailInput.classList.add("is-invalid");
+        return false;
+    }
+    validateForm();
+}
+
+function validateAIUEmail() {
+    var emailInput = document.getElementById("aiuEmail");
+    var validationMessage = document.getElementById(
+        "AIUemailValidationMessage"
+    );
+    var submitButton = document.getElementById("submitButton");
+
+    if (emailInput.validity.valid) {
+        validationMessage.innerText = "";
+        emailInput.classList.remove("is-invalid");
+        return true;
+    } else {
+        validationMessage.innerText = "Invalid email address";
+        emailInput.classList.add("is-invalid");
+        return false;
+    }
+    validateForm();
+}
+
+function validateName() {
+    var nameInput = document.getElementById("aipName");
+    var name = nameInput.value.trim();
+    var submitBtn = document.querySelector(".submit");
+    var vm = document.getElementById("nameValidationMessage");
+
+    if (name.length > 100) {
+        vm.innerText = "Invalid Length";
+        return false;
+    } else {
+        vm.innerText = "";
+        return true;
+    }
+    validateForm();
+}
+function validateAIUName() {
+    var nameInput = document.getElementById("aiuName");
+    var name = nameInput.value.trim();
+    var submitBtn = document.querySelector(".submit");
+    var vm = document.getElementById("AIUNameValidationMessage");
+
+    if (name.length > 100) {
+        vm.innerText = "Invalid Length";
+        return false;
+    } else {
+        vm.innerText = "";
+        return true;
+    }
+    validateForm();
+}
+
+function DPIDValidation() {
+    var dpidInput = document.getElementById("dpID").value;
+    var vm = document.getElementById("dpIDValidationMessage");
+
+    if (dpidInput.length !== 11 && dpidInput.length !== 12) {
+        vm.innerText = "Invalid ID";
+        return false;
+    } 
+    else {
+        if (dpidInput[0] == "T" || dpidInput[0] == "R") {
+            vm.innerText = "";
+            return true;
+        } else {
+            vm.innerText = "Invalid ID";
+            return false;
+        }
+    }
+    validateForm();
+}
+
+// added by sanjana--this fn
+function validateForm() {
+    // var formInputs = document.querySelectorAll("input");
+    // var isValid = true;
+
+    // formInputs.forEach(function (input) {
+    //     if (!input.validity.valid) {
+    //         isValid = false;
+    //         return;
+    //     }
+    // });
+
+    // if (isValid) {
+    //     link.setAttribute("href", "Summary.html");
+    // }
+    if (validateAIUEmail()&&validateAIUID()&&validateAIUName()&&validateEmail()&&validateName()&&validateUUID()&&DPIDValidation()) {
+        conJSON();
+        link.setAttribute("href","Summary.html");
+    }
+}
+
+function conJSON() {
+    console.log("a");
+    validateForm();
+        var aipID = document.getElementById("aipId").querySelector("input").value;
+        var aipEmail = document.getElementById("aipEmail").querySelector("input").value;
+        var aipName = document.getElementById("aipName").querySelector("input").value;
+        var aiuID = document.getElementById("aiuId").querySelector("input").value;
+        var aiuEmail = document.getElementById("aiuEmail").querySelector("input").value;
+        var aiuName = document.getElementById("aiuName").querySelector("input").value;
+        var dpID = document.getElementById("dpID").querySelector("input").value;
+        var dpName = document.getElementById("dpName").querySelector("input").value;
+        var itemType = document.getElementById("itemType").value;
+
+        var purpose = Array.from(document.getElementById("purpose").selectedOptions).map(function (option) {
+            return {
+                code: option.value,
+                name: option.textContent,
+            };
+        });
+        console.log("a");
+
+        var stuData = {
+            id: aipID,
+            aip: {
+                id: aipID,
+                email: aipEmail,
+                name: aipName,
+            },
+            aiu: {
+                id: aiuID,
+                email: aiuEmail,
+                name: aiuName,
+            },
+            dataPrincipal: {
+                id: dpID,
+                idType: itemType,
+                name: dpName,
+            },
+            purposes: purpose,
+            itemId: "datakaveri.org/a3dca9dfbe40f76b863da69d0d7d6f7c984e93bf/rs.iudx.io/ResGrp",
+            itemType: "resource_group",
+            expiry: "2024-05-31T13:05:36.471620Z",
+            createdAt: "2023-06-20T12:59:32.471620Z",
+        };
+        var jsonData = JSON.stringify(stuData);
+        console.log("a");
+        console.log(jsonData);
+        console.log("a");
+        // studentForm.reset();
+    }
+
